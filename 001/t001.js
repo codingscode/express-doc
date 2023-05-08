562b97ca1e12c61466f26e9dbc6a6847a766dc2c
codingscode <heronnestg@hotmail.com>
2023-05-08 10:14:40 -0300

    parte 2

--- 001/t001.js
+++ 001/t001.js
@@ -1,8 +1,26 @@
-const express = require('express')
+
+import express from 'express'
+
 const app = express()
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.send('GET request to the homepage')
 })
+
+// POST method route
+app.post('/', (req, res) => {
+    res.send('POST request to the homepage')
+})
+
+// App listening on the below port
+app.listen(3000, function(err){
+    if (err) console.log(err)
+    console.log('Servidor escutando na porta', 3000)
+ })
+
+
+
+
+
 
