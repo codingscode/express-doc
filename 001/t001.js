be5113807e4468c9274b43ec0609753cc6507b98
codingscode <heronnestg@hotmail.com>
2023-05-08 10:35:31 -0300

    parte 5

--- 001/t001.js
+++ 001/t001.js
@@ -7,21 +7,28 @@ app.use(express.json())
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => {
-  res.send('GET request to the homepages')
+  res.send('método get utilizado')
 })
 
 // POST method route
 app.post('/', (req, res) => {
-    const enviado = req.body
+    const valor_post = req.body
     res.send('método post utilizado')
-    console.log(req.body)
+    console.log(valor_post)
 })
 
 // App listening on the below port
 app.listen(3000, function(err){
     if (err) console.log(err)
     console.log('Servidor escutando na porta', 3000)
- })
+})
+
+
+app.all('/segredo', (req, res, next) => {
+    console.log('Acessando a seção segredo...')
+    next() // pass control to the next handler
+})
+
 
 
 
