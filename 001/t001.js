fff3d0d8ec498c9d740f82421fd53efbe4f30c83
codingscode <heronnestg@hotmail.com>
2023-05-08 10:19:45 -0300

    parte 3

--- 001/t001.js
+++ 001/t001.js
@@ -5,12 +5,12 @@ const app = express()
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => {
-  res.send('GET request to the homepage')
+  res.send('GET request to the homepages')
 })
 
 // POST method route
 app.post('/', (req, res) => {
-    res.send('POST request to the homepage')
+    res.send('método post utilizado')
 })
 
 // App listening on the below port
