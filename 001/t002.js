cfaf58ea38b44c6bfc2f0b157446ee8f01713c18
codingscode <heronnestg@hotmail.com>
2023-05-09 09:36:32 -0300

    parte 9

--- 001/t002.js
+++ 001/t002.js
@@ -22,7 +22,23 @@ const cb2 = (req, res) => {
 
 app.get('/exemplo/c', [cb0, cb1, cb2])
 
+const cb3 = function (req, res, next) {
+  console.log('CB3')
+  next()
+}
 
+const cb4 = function (req, res, next) {
+  console.log('CB4')
+  next()
+}
+
+app.get('/exemplo/d', [cb3, cb4], (req, res, next) => {
+  console.log('a resposta será enviada pela próxima função...')
+  next()
+  },
+  (req, res) => {
+    res.send('OLá de D!')
+})
 
 
 
