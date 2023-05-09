a376d1099e0625abc5289eb11c9467456b3e185f
codingscode <heronnestg@hotmail.com>
2023-05-09 09:43:14 -0300

    parte 10

---
+++ 001/t003.js
@@ -0,0 +1,38 @@
+
+import express from 'express'
+
+const app = express()
+
+app.use(express.json())
+
+
+app.route('/livro')
+  .get((req, res) => {
+    res.send('acessar um livro aleatório')
+  })
+  .post((req, res) => {
+    res.send('adicionar um livro')
+  })
+  .put((req, res) => {
+    res.send('atualizar umlivro')
+  })
+
+
+
+// App listening on the below port
+app.listen(3000, function(err){
+    if (err) console.log(err)
+    console.log('Servidor escutando na porta', 3000)
+})
+
+
+
+
+
+
+
+
+
+
+
+
