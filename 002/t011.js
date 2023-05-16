c1da8187913c52b81dea5767306458c204675114
codingscode <heronnestg@hotmail.com>
2023-05-16 11:59:38 -0300

    parte 22

---
+++ 002/t011.js
@@ -0,0 +1,23 @@
+
+import express from 'express'
+
+
+var app = express()
+
+app.use(express.json())
+
+
+var greet = express.Router()
+
+greet.get('/jp', function (req, res) {
+  console.log(req.baseUrl) // /greet
+  res.send('Konichiwa!')
+})
+
+app.use('/greet', greet) // load the router on '/greet'
+
+
+app.listen(3000, () => {
+   console.log('servidor executando na porta 3000')
+})
+
