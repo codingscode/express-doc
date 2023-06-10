da8728000a131b8cad1954d602ece1b467557adb
codingscode <heronnestg@hotmail.com>
2023-06-09 22:07:05 -0300

    parte 41

---
+++ 002/t031.js
@@ -0,0 +1,49 @@
+
+import express from 'express'
+
+
+
+var app = express()
+
+app.use(express.json()) // for parsing application/json
+
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+
+
+// http://localhost:3000
+const requestTime = (req, res, next) => {
+  req.requestTime = Date.now()
+  next()
+}
+
+app.use(requestTime)
+
+app.get('/', (req, res) => {
+  let responseText = 'Hello World!<br>'
+  responseText += `<small>Requested at: ${req.requestTime}</small>`
+  res.send(responseText)
+})
+
+app.listen(3000, () => {
+   console.log('servidor executando na porta 3000')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
