9ec44f6c81abb46b30089ff480ad7d6f46d217bd
codingscode <heronnestg@hotmail.com>
2023-06-09 22:14:09 -0300

    parte 42

---
+++ 002/t032.js
@@ -0,0 +1,51 @@
+
+import express from 'express'
+
+
+
+var app = express()
+
+app.use(express.json()) // for parsing application/json
+
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+
+
+// http://localhost:3000/usuario/9
+app.use((req, res, next) => {
+  console.log('Time:', Date.now())
+  next()
+})
+
+app.use('/usuario/:id', (req, res, next) => {
+  console.log('Request Type:', req.method)
+  next()
+})
+
+
+app.get('/usuario/:id', (req, res, next) => {
+  res.send('USER')
+})
+
+app.listen(3000, () => {
+   console.log('servidor executando na porta 3000')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
